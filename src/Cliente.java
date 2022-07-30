6fc9ca337e74865cc27a073dcf2821c5882e6984
MarceloVelasque <109104085+MarceloVelasque@users.noreply.github.com>
2022-07-29 22:31:49 -0300

    ExerciciosAula22

---
+++ src/Cliente.java
@@ -0,0 +1,45 @@
+package aula22Exercicio01;
+
+import java.security.PublicKey;
+
+public class Cliente extends Pessoa {
+
+	public Cliente(String nome) {
+		super(nome);
+		
+	}
+	private int codigo;
+	
+	public Cliente(String nome, int codigo) {
+		super(nome);
+		
+		this.codigo = codigo;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	@Override
+	public void imprimirDados() {
+		super.imprimirDados();
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [codigo=" + codigo + ", getCodigo()=" + getCodigo() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+		
+			
+		}
+	
+	
+	
+	
+
